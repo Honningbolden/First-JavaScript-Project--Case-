348a7f3d65f4f912dde689624d494bec873cd6f8
Honningbolden <hannibalmunch@gmail.com>
2023-09-07 16:58:05 +0200

    More changes

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-// const scopeButton = document.getElementById("button1")
-// const dopeButton = document.getElementById("button2")
-// const smokeButton = document.getElementById("button3")
+let doobieCounter = document.getElementById("doobieCounter");
+doobieCounter.innerText = doobieCount = 0;
 
-// let scopeBoolean = 0;
-// let dopeBoolean = 0;
-// let smokeBoolean = 0;
+const scopeButton = document.getElementById("button1")
+const dopeButton = document.getElementById("button2")
+const smokeButton = document.getElementById("button3")
 
-// scopeButton.addEventListener("click", scopeIt)
-// dopeButton.addEventListener("click", dopeIt)
-// smokeButton.addEventListener("click", smokeIt)
+let scopeBoolean = false;
+let dopeBoolean = false;
+let smokeBoolean = false;
 
-// function scopeIt() {
-//     console.log("scopeIt_Test")
-// }
+scopeButton.addEventListener("click", scopeIt)
+dopeButton.addEventListener("click", dopeIt)
+smokeButton.addEventListener("click", smokeIt)
 
-// function dopeIt() {
-//     console.log("dopeIt_Test")
-// }
-
-// function smokeIt() {
-//     console.log("smokeIt_Test")
-// }
-
-const buttons = ["scopeButton", "dopeButton", "smokeButton"]
-
-for (let i = 0; i <= buttons.length; i += 1) {
-    buttons[i] = document.getElementById(`button${i}`);
+function scopeIt() {
+    scopeBoolean = true;
 }
+
+function dopeIt() {
+    if (scopeBoolean === true) {
+        dopeBoolean = true;
+    }
+}
+
+function smokeIt() {
+    if (scopeBoolean === true && dopeBoolean === true) {
+        smokeBoolean = true;
+        checkConditions();
+    }
+}
+
+function checkConditions() {if (scopeBoolean === true && dopeBoolean === true && smokeBoolean === true) {
+    doobieCount += 1;
+    doobieCounter.innerText = doobieCount;
+    }
+};
